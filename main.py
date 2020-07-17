21a553e68fdb805d6ada5d2fe4f485fabd49fef9
Shravan Kumar Yadav <Shravan542316@gmail.com>
2020-07-17 23:15:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import discord, urllib.parse, requests, json
 from discord.ext import commands
 
-
 #Code - 0.75
-#Last Change - hid sensitive info.
+#Last Change - Added actor search.
 
+
+# This will search for a file named 'token_key.txt' in the same directory where 'main.py' is (unless you specify otherwise) and
+# store the text in its first line in a variable named 'token' and the text in its second line in a variable named 'api_key'. 
 file = open('token_key.txt', 'r')
 token = file.readline()
 api_key = file.readline()
 file.close()
 
 
-
+# First line tells the program what our prefix is going to be, you can edit it if you want. The second line removes the default 'help'
+# command that comes integrated with discord.py and allows you to have your own custom 'help' command.
 client = commands.Bot(command_prefix = 'tv!')
 client.remove_command('help')
 
 
-
+# The code that get executed as soon as your bot comes online, right now it sets its status as 'playing a game' and prints 'Online as {bot name}.
 @client.event
 async def on_ready():
     game = discord.Game("innocent. Type - tv!help.")
@@ -24,12 +27,13 @@ async def on_ready():
     print('Online as {0.user}'.format(client))
 
 
-
+# Response to 'tv!ping' command.
 @client.command()
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency*1000)}ms')
 
 
+# Resposne to 'tv!help', it is an embedded message as all of our next resposnes are going to be, see this page - 'https://cog-creators.github.io/discord-embed-sandbox/'
 @client.command()
 async def help(ctx):
     embed=discord.Embed(title="Visit TMDb website.", url="https://www.themoviedb.org/", description="This product uses the TMDb API but is not endorsed or certified by TMDb.")
@@ -43,12 +47,13 @@ async def help(ctx):
     await ctx.send(embed=embed)
 
 
+
+# 'movie/movies' command, it only prints out 3 results maximum since the number of results can vary and I couldn't figure out how to deal with that.
 @client.command(aliases=['movies'])
 async def movie(ctx, *, movie_search_str):
-    encoded_search_str = urllib.parse.quote(movie_search_str)
+    encoded_search_str = urllib.parse.quote(movie_search_str)    #URL-encodes the search string.
     response = requests.get(f'https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={encoded_search_str}&page=1&include_adult=false')
-    movie_result_dict = response.json()
-
+    movie_result_dict = response.json()      #To understand how I am fetching values from the response dictionary, make a separate program and print the json response.
     if (movie_result_dict['total_results']) == 0:
         await ctx.send('Your search returned 0 results.')
 
@@ -97,7 +102,7 @@ async def movie(ctx, *, movie_search_str):
         await ctx.send(embed=embed1)
         await ctx.send(embed=embed2)
 
-    else:
+    else:  # since the number of results is not 0, 1, or 2, it must be 3 or more, in this case, we can safely fetch values for first 3 results without checking how many results are there.
         movie_title1 = ((movie_result_dict.get('results')[0])['title'])
         movie_id1 = ((movie_result_dict.get('results')[0])['id'])
         movie_desc1 = ((movie_result_dict.get('results')[0])['overview'])
@@ -140,7 +145,7 @@ async def movie(ctx, *, movie_search_str):
         await ctx.send(embed=embed2)
         await ctx.send(embed=embed3)
 
-
+# same as above, just tv shows instead of movies.
 @client.command(aliases=['tvshows'])
 async def tvshow(ctx, *, tv_search_str):
     encoded_search_str = urllib.parse.quote(tv_search_str)
@@ -248,24 +253,26 @@ async def tvshow(ctx, *, tv_search_str):
         await ctx.send(embed=embed3)
 
 
+        
+# This only prints 1 result
 @client.command(aliases=['actor', 'actors'])
 async def person(ctx, *, actor_search_str):
     encoded_search_str = urllib.parse.quote(actor_search_str)
     response = requests.get(f'https://api.themoviedb.org/3/search/person?api_key={api_key}&query={encoded_search_str}&page=1&include_adult=false')
     actor_result_dict = response.json()
 
-    if (actor_result_dict.get('total_results')) == 0 or ((actor_result_dict.get('results')[0])['known_for_department']) != 'Acting':
+    if (actor_result_dict.get('total_results')) == 0 or ((actor_result_dict.get('results')[0])['known_for_department']) != 'Acting':   # along with confirming that the response dictionary is not empty, I am also making sure the person is an actor.
         await ctx.send('Your search returned 0 results.')
 
     else:
         actor_id = ((actor_result_dict.get('results')[0])['id'])
-        id_query_response = requests.get(f'https://api.themoviedb.org/3/person/{actor_id}?api_key={api_key}&language=en-US')
+        id_query_response = requests.get(f'https://api.themoviedb.org/3/person/{actor_id}?api_key={api_key}&language=en-US')     # The search api endpoint does not get you their biographies so you have to make another request on person details API.
         actor_info_dict = id_query_response.json()
 
         actor_name = ((actor_result_dict.get('results')[0])['name'])
         bio = (actor_info_dict.get('biography'))
         
-        if (len(bio)) > 2048:
+        if (len(bio)) > 2048:                   # The discription body of an embedded message has a limit of 2048 characters, this code slices out a portion of biography to make sure it fits in.
             actor_bio = (bio[0:2030] + '.....')
         else:
             actor_bio = bio
@@ -285,14 +292,8 @@ async def person(ctx, *, actor_search_str):
 
 
 
-@client.command(aliases = ['popularmovies'])
-async def popularmovie(ctx):
-    await ctx.send ('ok lol')
 
 
-
-
-
-
+# That was it, have a good day!
 
 client.run(token)
